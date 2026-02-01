460049101cc5f1075952b0ffd98986ab845e4225
Hieu Dao <HieuDao-code@users.noreply.github.com>
2026-02-01 20:40:52 +0100

    Add comments

--- flake.nix
+++ flake.nix
@@ -2,8 +2,9 @@
   description = "A simple NixOS flake";
 
   inputs = {
-    # NixOS official package source, using the nixos-25.11 branch here
+    # NixOS official package source, using the stable nixos-25.11 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    # We use the unstable nixpkgs repo for some packages
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     # # home-manager, used for managing user configuration
     # home-manager = {
