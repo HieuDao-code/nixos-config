8e5892207a97c10483da5158dfd88c0b620208e7
Hieu Dao <HieuDao-code@users.noreply.github.com>
2026-02-09 21:37:35 +0100

    home.nix cleanup

--- home.nix
+++ home.nix
@@ -114,54 +114,12 @@
   #   usbutils # lsusb
   # ];
 
-  # # basic configuration of git, please change to your own
+  # # Basic git configuration
   # programs.git = {
   #   enable = true;
-  #   userName = "Hieu Dao";
-  #   userEmail = "HieuDao-code@users.noreply.github.com";
-  # };
-  #
-  # # starship - an customizable prompt for any shell
-  # programs.starship = {
-  #   enable = true;
-  #   # custom settings
   #   settings = {
-  #     add_newline = false;
-  #     aws.disabled = true;
-  #     gcloud.disabled = true;
-  #     line_break.disabled = true;
+  #     user.name = "Hieu Dao";
+  #     user.email = "HieuDao-code@users.noreply.github.com";
   #   };
   # };
-
-  # alacritty - a cross-platform, GPU-accelerated terminal emulator
-  # programs.alacritty = {
-  #   enable = true;
-  #   # custom settings
-  #   settings = {
-  #     env.TERM = "xterm-256color";
-  #     font = {
-  #       size = 12;
-  #       draw_bold_text_with_bright_colors = true;
-  #     };
-  #     scrolling.multiplier = 5;
-  #     selection.save_to_clipboard = true;
-  #   };
-  # };
-  #
-  # programs.bash = {
-  #   enable = true;
-  #   enableCompletion = true;
-  #   # TODO add your custom bashrc here
-  #   bashrcExtra = ''
-  #     export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
-  #   '';
-  #
-  #   # set some aliases, feel free to add more or remove some
-  #   shellAliases = {
-  #     k = "kubectl";
-  #     urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
-  #     urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
-  #   };
-  # };
-
 }
