ecd7228a30850ee842506bb234623a396ab1e702
Hieu Dao <HieuDao-code@users.noreply.github.com>
2026-02-11 21:48:19 +0100

    niri

--- configuration.nix
+++ configuration.nix
@@ -64,8 +64,9 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.displayManager.gdm.enable = true;
-  services.desktopManager.gnome.enable = true;
+  # services.displayManager.autoLogin.enable = true;
+  services.displayManager.ly.enable = true;
+  # services.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
